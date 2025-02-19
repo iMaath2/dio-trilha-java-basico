50c9d11d3e35199d07b1348a4ca420c8aa27c0ba
iMaath2 <imaath2@gmail.com>
2025-02-19 04:47:38 -0300

    Praticando comentarios e tags

---
+++ Calculaddora/src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    /**
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return retorna o valor da soma dos dois parâmetros
+     **/
+    public int somar (int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
